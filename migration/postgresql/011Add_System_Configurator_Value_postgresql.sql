691df0b4eaaef199766ad491d45ffcef377eefc0
Yamel Senih <Yamel Senih@localhost>
2013-04-18 20:38:29 -0530

    Delete Old Migrations

--- migration/postgresql/011Add_System_Configurator_Value_postgresql.sql
+++
@@ -1,5 +0,0 @@
--- Apr 1, 2013 8:40:16 PM VET
--- Get Weight from scale
-INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (11,0,3000203,'C',TO_TIMESTAMP('2013-04-01 20:40:15','YYYY-MM-DD HH24:MI:SS'),100,'ECA03','Y','OPEN_PORT_AUTOMATIC_IN_GETWEIGHT',TO_TIMESTAMP('2013-04-01 20:40:15','YYYY-MM-DD HH24:MI:SS'),100,'N')
-;
-
